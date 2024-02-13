39885f1e39a1d8835aebebb9803fd0e6d5b5dc7a
careofyou <fedor.trojk@gmail.com>
2024-02-13 09:56:17 +0300

    initial

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "../Button/Button";
 import { useTelegram } from "../../hooks/useTelegram";
+import './Header.css'
 
 const Header = () => {
     const {user, onClose} = useTelegram();
@@ -9,10 +10,10 @@ const Header = () => {
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
-                {tg.initDataUnsafe?.user?.username} 
+                {user?.username}
             </span>
         </div>
-    ) 
-}
+    );
+};
 
 export default Header;
